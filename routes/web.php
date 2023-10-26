9fc16e144c5a2c2c4a1b47c846db013d8b4ffe63
zaani12 <zaani.hamza.solicode.com>
2023-10-26 15:04:50 +0100

    Routing

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/bloge',function(Request $Request){
-
     return [
-        "name" => $Request -> path(),
-        "article"=> "Article 1"
+        "link" => \route('bloge.show',['slug'=>'article','id'=> 13]),
     ];
-});
+})->name('blog.index');
+// Route::get('/bloge',function(Request $Request){
+
+//     return [
+//         "name" => $Request -> path(),
+//         "article"=> "Article 1"
+//     ];
+// });
+// use Illuminate\Http\Request;
+
+Route::get('/blog/{id}-{slug}', function (string $slug, string $id ) {
+        return [
+            "slug" => $slug,
+            "id" => $id,
+           "name" => "Tanger",
+    ];
+})->name('blog.show');
