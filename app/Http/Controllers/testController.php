7e339b2b81503b16b45b8795b8d62b99c772a001
zaani12 <zaani.hamza.solicode.com>
2023-11-02 22:56:27 +0100

    validat data using class validator

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -6,15 +6,29 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Nette\Utils\Paginator;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\validator;
 class testController extends Controller
 {
     public function index() :view{
+       $validator = validator::make([
+            "test"=> "agddr",
+            "content"=> "fdr",
+        ], [
+            "test"=> "required |min:4 ",
+            "content"=> "required |min:4 ",
+
+        ]);
+        // dd($validator->fails()); // return true or fals 
+        // dd($validator->errors()); // return array of error messages 
+        dd($validator->validated());
         // $tables = tables1::paginate(3);
         // return (['tables' => $tables]);
         return view('test.index',[
             $tables = tables1::paginate(1),
             'tables' => $tables
         ]);
+        
+
     }
   
     public function show(string $test , string $id): RedirectResponse | tables1
