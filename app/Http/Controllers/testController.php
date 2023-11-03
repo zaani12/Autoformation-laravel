f24185b06a937250806a24fc4c0a24e2a6f545f1
zaani12 <zaani.hamza.solicode.com>
2023-11-03 16:47:50 +0100

    validation

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -1,20 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Requests\TestFilterRequest;
 use App\Models\tables1;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Nette\Utils\Paginator;
 use Illuminate\View\View;
+
+
 class testController extends Controller
 {
-    public function index() :view{
+    public function index(TestFilterRequest $request) :view{
         // $tables = tables1::paginate(3);
         // return (['tables' => $tables]);
+        // dd($request->validated());
         return view('test.index',[
             $tables = tables1::paginate(1),
             'tables' => $tables
         ]);
+
     }
   
     public function show(string $test , string $id): RedirectResponse | tables1
