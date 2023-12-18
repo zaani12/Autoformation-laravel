0085a6273f2839047d415617eee9adf748a4c34e
zaani12 <zaani.hamza.solicode@gmail.com>
2023-12-18 10:04:21 +0100

    add messege

--- resources/views/test/base.blade.php
+++ resources/views/test/base.blade.php
@@ -22,11 +22,7 @@
   </li>
 </ul>
 <div class="container">
-@if($session('success'))
-        <div class="alert alert-success">
-            {{ ($session('success')) }}
-        </div>
-    @endif
+
     @yield('content')
 </div>
 </body>
